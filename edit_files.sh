f7bb3088f999b6067f587ff45e82464e2f0677f1
[marinipete] <pedro.marini@compasso.com.br>
2018-12-10 14:22:28 -0200

    Added new code to edit_files.sh

--- edit_files.sh
+++ edit_files.sh
@@ -15,7 +15,7 @@ Main_edit() {
    echo -e "\033[0;32m 1. Logstash \033[0m"
    echo -e "\033[0;32m 2. Kibana \033[0m"
    echo -e "\033[0;32m 3. Packetbeat \033[0m"
-   echo -e "\033[0;32m 5. Filebeat \033[0m"
+   echo -e "\033[0;32m 4. Filebeat \033[0m"
    echo -e "\033[0;32m 5. Metricbeat \033[0m"
    echo -e "\033[0;32m 6. Heartbeat \033[0m"
    echo -e "\033[0;32m 7. Voltar \033[0m"
@@ -50,23 +50,59 @@ Opt1 () {
     echo -e "\033[0;32m 5. pipeline-packetbeat.cfg \033[0m"
     echo -e "\033[0;32m 6. Voltar \033[0m"
     printf "\n"
-    echo -en "\033[0;32m R: \033[0m";read edt_lgt
-
-    #!!ALTERAR PARA UM ELIF NO FINAL (SEM CONDICIONAL) - O QUE FAZER DEPOIS DO VIM ?!!
-    if [ "$edt_lgt" = "1" ]
-        then cp -n ${path_config}/logstash.yml ${path_config}/bkp/logstash.yml_`date +%d.%m.%Y-%Hh:%Mmin` && vim    ${path_config}/logstash.yml && Opt1
-        elif [ "$edt_lgt" = "2" ]
+    echo -en "\033[0;32m R: \033[0m";read edit_file
+    
+    #BKP and edit files
+    if [ "$edit_file" = "1" ]
+        then cp -n ${path_config}/${produto}.yml ${path_config}/bkp/${produto}.yml_`date +%d.%m.%Y-%Hh:%Mmin` && vim    ${path_config}/${produto}.yml && Opt1
+        elif [ "$edit_file" = "2" ]
         then cp -n ${path_config}/pipelines.yml ${path_config}/bkp/pipelines.yml_`date +%d.%m.%Y-%Hh:%Mmin` &&  vim ${path_config}/pipelines.yml && Opt1
-        elif [ "$edt_lgt" = "3" ]
+        elif [ "$edit_file" = "3" ]
         then cp -n ${path_config}/jvm.options ${path_config}/bkp/jvm.options_`date +%d.%m.%Y-%Hh:%Mmin` && vim $    {path_config}/jvm.options && Opt1
-        elif [ "$edt_lgt" = "4" ]
+        elif [ "$edit_file" = "4" ]
         then cp -n ${path_config}/pipeline-canais.cfg ${path_config}/bkp/pipeline-canais.cfg_`date  +%d.%m.%Y-%Hh:%Mmin` && vim ${path_config}/pipeline-canais.cfg && Opt1
-        elif [ "$edt_lgt" = "5" ]
+        elif [ "$edit_file" = "5" ]
         then cp -n ${path_config}/pipeline-packetbeat.cfg ${path_config}/bkp/pipeline-packetbeat.cfg_`date  +%d.%m.%Y-%Hh:%Mmin` && vim ${path_config}/pipeline-packetbeat.cfg && Opt1
-        elif [ "$edt_lgt" = "6" ]
+        elif [ "$edit_file" = "6" ]
         then Main_edit
         else echo -e "\033[0;31m Opcão inválida. \033[0m" ; sleep 1; clear
     fi
+}
+
+#Edit Kibana's files
+Opt2 () {
+    produto=kibana
+    path_config=${path_produto}/${produto}/config
+    cp -n ${path_config}/${produto}.yml ${path_config}/bkp/${produto}.yml_`date +%d.%m.%Y-%Hh:%Mmin` && vim    ${path_config}/${produto}.yml && Main_edit
+}
+
+#Edit Packetbeat's files
+Opt3 () {
+    produto=packetbeat
+    path_config=${path_produto}/${produto}/config
+    cp -n ${path_config}/${produto}.yml ${path_config}/bkp/${produto}.yml_`date +%d.%m.%Y-%Hh:%Mmin` && vim    ${path_config}/${produto}.yml && Main_edit
+}
+
+#Edit Filebeat's files
+Opt4 () {
+#    produto=filebeat
+#    path_config=${path_produto}/${produto}/config
+#    cp -n ${path_config}/${produto}.yml ${path_config}/bkp/${produto}.yml_`date +%d.%m.%Y-%Hh:%Mmin` && vim    ${path_config}/${produto}.yml && Main_edit
+echo -e "\033[0;31m Em desenvolvimento. \033[0m" ; sleep 1; Main_edit
+}
+
+#Edit Metricbeat's files
+Opt5 () {
+    produto=metricbeat
+    path_config=${path_produto}/${produto}/config
+    cp -n ${path_config}/${produto}.yml ${path_config}/bkp/${produto}.yml_`date +%d.%m.%Y-%Hh:%Mmin` && vim    ${path_config}/${produto}.yml && Main_edit
+}
+
+#Edit Heartbeat's files
+Opt6 () {
+    produto=heartbeat
+    path_config=${path_produto}/${produto}/config
+    cp -n ${path_config}/${produto}.yml ${path_config}/bkp/${produto}.yml_`date +%d.%m.%Y-%Hh:%Mmin` && vim    ${path_config}/${produto}.yml && Main_edit
 }
 
 Main_edit
